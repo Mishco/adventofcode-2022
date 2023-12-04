16b99785ca0b0ac0551b23a223ae496ce28fdab2
misl <michal.slovik@tutanota.com>
2023-12-04 10:28:49 +0100

    Add second day

--- day-2/main.py
+++ day-2/main.py
@@ -2,19 +2,25 @@
 # X for Rock, Y for Paper, and Z for Scissors
 
 # score:
-# shape you selected (1 for Rock, 2 for Paper, and 3 for Scissors)
-# + outcome of the round (0 if you lost, 3 if the round was a draw, and 6 if you won)
 ii = """A Y
 B X
 C Z
 """
 
+# shape you selected (1 for Rock, 2 for Paper, and 3 for Scissors)
 shapes = {
     'X': 1,
     'Y': 2,
     'Z': 3
 }
+# X means you need to lose, Y means you need to end the round in a draw, and Z means you win
+another_shapes = {
+    'X': 0,
+    'Y': 3,
+    'Z': 6
+}
 
+# + outcome of the round (0 if you lost, 3 if the round was a draw, and 6 if you won)
 scores = {
     ('A', 'X'): 3,
     ('A', 'Y'): 6,
@@ -35,3 +41,24 @@ for row in content.splitlines():
     score += scores.get((first_col, second_col), 0) + shape
 
 print(score)  # 11603
+
+# part 2
+points = {
+    "A X": 0 + 3,
+    "A Y": 3 + 1,
+    "A Z": 6 + 2,
+    "B X": 0 + 1,
+    "B Y": 3 + 2,
+    "B Z": 6 + 3,
+    "C X": 0 + 2,
+    "C Y": 3 + 3,
+    "C Z": 6 + 1,
+}
+rounds = content.split("\n")
+scores = []
+total = 0
+for one_round in rounds:
+    scores.append(points[one_round])
+    total += points[one_round]
+
+print(total) # 12725
