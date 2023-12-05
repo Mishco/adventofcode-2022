02d0004dda611fd6ebaa07d24e65244535bc4f6d
misl <michal.slovik@tutanota.com>
2023-12-05 15:43:53 +0100

    Updates

--- day-5/main.py
+++ day-5/main.py
@@ -21,27 +21,3 @@ for line in instructions:
 print("Part 1:", "".join(s[0] for s in p1 if s))
 print("Part 2:", "".join(s[0] for s in p2 if s))
 
-# crates = c.split('move')[0]
-# moves = c.split('move')[1:]
-# print(crates)
-# print(moves)
-# s = []
-# for line in x:
-#     if line == "\n":
-#         break
-#     s.append([line[k * 4 + 1] for k in range(len(line) // 4)])  # this is just line[1::4] - thanks UnrelatedString
-#
-# # print(s)
-# s.pop()
-# s = [list("".join(c).strip()[::-1]) for c in zip(*s)]
-#
-# # print(s)
-#
-# for line in x:
-#     a, b, c = map(int, re.findall("\\d+", line))
-#
-#     s[c - 1].extend(s[b - 1][-a:][::-1])
-#     s[b - 1] = s[b - 1][:-a]
-#
-# print(s)
-# # print(which[-1] for )
